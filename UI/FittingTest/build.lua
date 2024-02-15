b4ecac92f80b22d9542bb1db0e76acaa2eb76a65
tbolzhel <01176130@pw.edu.pl>
2024-02-15 20:12:05 +0100

    Tester window

---
+++ UI/FittingTest/build.lua
@@ -0,0 +1,45 @@
+project "FittingTest"
+	kind "ConsoleApp"
+    language "C++"
+
+    location "%{wks.location}/FittingTest"
+    staticruntime(srunt)
+    files {
+        "%{prj.location}/**.cpp",
+        "%{prj.location}/**.h"
+    }
+
+    removefiles { "%{prj.location}/implot_demos/**.*" }
+
+    includedirs { "%{prj.location}/include", 
+                  "%{wks.location}/Vendor/imgui",
+                  "%{wks.location}/Vendor/imgui/backends",
+                  "%{wks.location}/Vendor/imgui/misc/cpp",
+                  "%{wks.location}/Vendor/implot",
+                  "%{wks.location}/Vendor/libs/glfw/include"
+    }
+    targetdir ( "%{wks.location}/bin/%{prj.name}-%{cfg.buildcfg}" )
+    objdir ( "%{wks.location}/obj/%{cfg.buildcfg}" )
+
+
+
+    
+    libdirs ( "%{wks.location}/lib" )
+
+
+    filter { "system:windows", "platforms:x86" }
+		libdirs { "%{wks.location}/Vendor/libs/glfw/lib-vc2010-32" }
+
+	filter { "system:windows", "platforms:x86_64" }
+        libdirs { "%{wks.location}/Vendor/libs/glfw/lib-vc2010-64" }
+
+    filter { "configurations:Debug" }
+        runtime "Debug"
+    filter { "configurations:Release" }
+        runtime "Release"
+
+    filter { "system:windows" }
+		ignoredefaultlibraries { "msvcrt" }
+        links {  "legacy_stdio_definitions", "opengl32", "glfw3", "imgui", "implot" }
+
+
---
+++ UI/FittingTest/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "tester.h"
+
+
+int main() {
+
+	Tester win{};
+
+	win.init();
+	win.run();
+
+
+	return 0;
+}
