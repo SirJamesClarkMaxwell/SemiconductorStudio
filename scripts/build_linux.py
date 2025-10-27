f0f2cb0be73da768dad5556f1d0933b53a847379
cyberCuber07 <n.eigocuber@gmail.com>
2025-10-27 18:58:41 +0100

    [feat] Remove NumericStorm patch
    Now have commiter rights to NumericStorm repo
    so instead pushing there changes directly.
    
    Signed-off-by: cyberCuber07 <n.eigocuber@gmail.com>

--- scripts/build_linux.py
+++ scripts/build_linux.py
@@ -67,8 +67,7 @@ def setup_dependencies(doClean):
     deps = [
         ['LambertW', 'https://github.com/SirJamesClarkMaxwell/LambertW', 'bf728a4',
             ['0001-feat-Add-build-script.patch'], do_meson_build],
-        ['NumericStorm', 'https://github.com/SirJamesClarkMaxwell/NumericStorm', 'f80187b',
-            ['0001-fix-Make-compile-on-unix-systems.patch'], None],
+        ['NumericStorm', 'https://github.com/SirJamesClarkMaxwell/NumericStorm', 'f80187b', [], None],
         ['googletest', 'https://github.com/ArnoXX/googletest', 'ff233bd', [], None],
         ['imgui', 'https://github.com/ArnoXX/imgui', 'c795886',
             ['0001-fix-Make-compile-on-unix-systems.patch'], do_meson_build],
