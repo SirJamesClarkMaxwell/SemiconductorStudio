c694c47e2249bafdf10fc2eb5919340134bf2f5f
SirJamesClarkMaxwell <fzabier@gmail.com>
2026-02-19 20:40:54 +0100

    Revert "[feat] Code cleanup (Part 1)"
    This reverts commit 2ab4169f6cc5b3a7cc3bc4e03ff0f31b4cc8f736.

--- JFMServices/MonteCarlo/MonteCarloEngine.hpp
+++ JFMServices/MonteCarlo/MonteCarloEngine.hpp
@@ -3,7 +3,6 @@
 #include "../Fitting/JFMIFitting.hpp"
 #include "../Fitting/JFMFitter.hpp"
 #include "../Fitting/PreFitter.hpp"
-#include <atomic>
 
 namespace JFMService
 {
@@ -34,10 +33,12 @@ namespace JFMService
         Fitters::Fitter m_fitter;
         PreFitter m_prefitter;
 
-        inline static thread_local std::mt19937 generator{std::random_device{}()};
+        // inline thread_local static std::mt19937 m_generator;
+        inline static thread_local std::mt19937 m_generator{std::random_device{}()};
 
     private:
         int calculateDegreesOfFreedom(const ParameterMap &trueParameters, const ParameterMap &fixedValues);
+        //UncertaintyMultipliers initializeUncertaintyMultipliers();
         double getUncertaintyMultiplier(uint8_t numberOfParameters, ConfidenceLevel level);
 
         double calculateMaximumError(const PlotData &trueData, double noiseFactor);
@@ -47,8 +48,5 @@ namespace JFMService
         void simulateChunk(int startIdx, int chunkSize, const std::shared_ptr<AbstractPreFit> &preFitter,
                            const std::shared_ptr<Fitters::AbstractFitter> fitter, MCInput &input,
                            std::vector<MCResult> &localResults, int numBlock);
-
-        std::atomic<uint32_t> m_iterationCount;
-        std::atomic<uint32_t> m_blockNumber;
     };
 }
